fa137db547930b00edb6e0c6307bfc05eef7f709
Tamim Zaman <tamimzaman333@gmail.com>
2021-09-13 00:29:24 +0600

    Update __main__.py

--- GabiBraunRobot/__main__.py
+++ GabiBraunRobot/__main__.py
@@ -55,7 +55,7 @@ def get_readable_time(seconds: int) -> str:
 PM_START_TEXT = """
 `Pika Pika!` [👩‍💼](https://telegra.ph/file/0d79e4dd2abdf6779ce16.jpg) `My name is` *PikaChu*
 `I am an Anime Themed group management bot.
-Build by The Ghost Hunter and managed by RYUK for Your Telegram Group , I specialize in managing anime and similar themed groups.
+Build by [RYUK](tg://openmessage?user_id=1281033576) and managed by [Tamim](tg://openmessage?user_id=1157847739) for Your Telegram Group , I specialize in managing anime and similar themed groups.
 You can find my list of available commands with! Hit` *🔐Commands*   
 """
 
