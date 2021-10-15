1c7faad0b5836e05d27444a58dfb556578f71820
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-10-15 07:52:19 +0700

    Fixed

--- GabiBraunRobot/modules/mention.py
+++ GabiBraunRobot/modules/mention.py
@@ -3,78 +3,62 @@
 # github account : https://github.com/ultroi/
 # This file is part of Pikachu (Telegram Bot)
 
+import os
+import logging
+import asyncio
 
-import os, logging, asyncio
 from telethon import Button
-from telethon import TelegramClient, events
-from telethon.sessions import StringSession
+from telethon import events
 from telethon.tl.types import ChannelParticipantsAdmins
-from GabiBraunRobot.events import register as GabiBraunRobot
 
+from GabiBraunRobot import telethn
 
-
-
-@GabiBraunRobot(pattern="^/all ?(.*)")
+@telethn.on(events.NewMessage(pattern="^/mentionall ?(.*)"))
 async def mentionall(event):
-    if event.is_private:
-        return await event.respond(
-            "__This command can be use in groups and channels!__"
-        )
-
-    admins = []
-    async for admin in client.iter_participants(
-        event.chat_id, filter=ChannelParticipantsAdmins
-    ):
-        admins.append(admin.id)
-    if not event.sender_id in admins:
-        return await event.respond("__Only admins can mention all!__")
-
-    if event.pattern_match.group(1):
-        mode = "text_on_cmd"
-        msg = event.pattern_match.group(1)
-    elif event.reply_to_msg_id:
-        mode = "text_on_reply"
-        msg = event.reply_to_msg_id
-        if msg == None:
-            return await event.respond(
-                "__I can't mention members for older messages! (messages which sended before i added to group)__"
-            )
-    elif event.pattern_match.group(1) and event.reply_to_msg_id:
-        return await event.respond("__Give me one argument!__")
-    else:
-        return await event.respond(
-            "__Reply to a message or give me some text to mention others!__"
-        )
-
-    if mode == "text_on_cmd":
+  if event.is_private:
+    return await event.respond("__This command can be use in groups and channels!__")
+  
+  admins = []
+  async for admin in telethn.iter_participants(event.chat_id, filter=ChannelParticipantsAdmins):
+    admins.append(admin.id)
+  if not event.sender_id in admins:
+    return await event.respond("Only admins can mention all!")
+  
+  if event.pattern_match.group(1):
+    mode = "text_on_cmd"
+    msg = event.pattern_match.group(1)
+  elif event.reply_to_msg_id:
+    mode = "text_on_reply"
+    msg = event.reply_to_msg_id
+    if msg is None:
+        return await event.respond("__I can't mention members for older messages! (messages which sended before i added to group)__")
+  elif event.pattern_match.group(1) and event.reply_to_msg_id:
+    return await event.respond("__Give me one argument!__")
+  else:
+    return await event.respond("__Reply to a message or give me some text to mention others!__")
+  
+  if mode == "text_on_cmd":
+    usrnum = 0
+    usrtxt = ""
+    async for usr in telethn.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+      if usrnum == 5:
+        await telethn.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
+        await asyncio.sleep(2)
         usrnum = 0
         usrtxt = ""
-        async for usr in client.iter_participants(event.chat_id):
-            usrnum += 1
-            usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-            if usrnum == 5:
-                await client.send_message(event.chat_id, f"{usrtxt}\n\n{msg}")
-                await asyncio.sleep(2)
-                usrnum = 0
-                usrtxt = ""
-
-    if mode == "text_on_reply":
+        
+  if mode == "text_on_reply":
+    usrnum = 0
+    usrtxt = ""
+    async for usr in telethn.iter_participants(event.chat_id):
+      usrnum += 1
+      usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
+      if usrnum == 5:
+        await telethn.send_message(event.chat_id, usrtxt, reply_to=msg)
+        await asyncio.sleep(2)
         usrnum = 0
         usrtxt = ""
-        async for usr in client.iter_participants(event.chat_id):
-            usrnum += 1
-            usrtxt += f"[{usr.first_name}](tg://user?id={usr.id}) "
-            if usrnum == 5:
-                await client.send_message(event.chat_id, usrtxt, reply_to=msg)
-                await asyncio.sleep(2)
-                usrnum = 0
-                usrtxt = ""
-
-__help__ = """
-
-Here this modulenis help for Mention Command :- 
-
-/all - use to tag all users in grp 
-
-"""
-__mod_name__ = "Mention"
+        
+__mod_name__ = "Mention All"
