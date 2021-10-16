245da286601b68ea1eda452f0bdd108bd98ffe04
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-10-16 07:10:40 +0700

    Fixed

--- GabiBraunRobot/modules/telegraph.py
+++ GabiBraunRobot/modules/telegraph.py
@@ -1,14 +1,14 @@
 from GabiBraunRobot.events import register
 from GabiBraunRobot import telethn as tbot
-TMP_DOWNLOAD_DIRECTORY = "tg-File/"
+TMP_DOWNLOAD_DIRECTORY = "./"
 from telethon import events
 import os
 from PIL import Image
 from datetime import datetime
 from telegraph import Telegraph, upload_file, exceptions
-babe = "PikachuRobo_bot"
+Oraizon = "Pikachu"
 telegraph = Telegraph()
-r = telegraph.create_account(short_name=babe)
+r = telegraph.create_account(short_name=Oraizon)
 auth_url = r["auth_url"]
 
 
@@ -41,7 +41,7 @@ async def _(event):
                 end = datetime.now()
                 ms_two = (end - start).seconds
                 os.remove(downloaded_file_name)
-                await h.edit("Uploaded to [Telegraph](https://telegra.ph{}) in {} seconds.".format(media_urls[0], (ms + ms_two)), link_preview=True)
+                await h.edit("Uploaded to https://telegra.ph{})".format(media_urls[0]), link_preview=True)
         elif input_str == "xt":
             user_object = await tbot.get_entity(r_message.sender_id)
             title_of_page = user_object.first_name # + " " + user_object.last_name
@@ -69,7 +69,7 @@ async def _(event):
             )
             end = datetime.now()
             ms = (end - start).seconds
-            await event.reply("Pasted to [Telegraph](https://telegra.ph/{}) in {} seconds.".format(response["path"], ms), link_preview=True)
+            await event.reply("Pasted to https://telegra.ph/{} in {} seconds.".format(response["path"], ms), link_preview=True)
     else:
         await event.reply("Reply to a message to get a permanent telegra.ph link.")
 
@@ -78,10 +78,11 @@ def resize_image(image):
     im = Image.open(image)
     im.save(image, "PNG")
 
-file_help = os.path.basename(__file__)
-file_help = file_help.replace(".py", "")
-file_helpo = file_help.replace("_", " ")
 
- 
+__help__ = """
+I can upload files to Telegraph
+ ❍ /tm :Get Telegraph Link Of Replied Media
+ ❍ /txt :Get Telegraph Link of Replied Text
+"""
 
- 
+__mod_name__ = "Telegraph"
