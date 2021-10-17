6f48ba1ce8f8830cd09b81ed93cccba8b53dd850
Arya Zakaria <84753790+aryazakaria01@users.noreply.github.com>
2021-10-17 15:13:42 +0700

    Fixed

--- GabiBraunRobot/modules/paste.py
+++ GabiBraunRobot/modules/paste.py
@@ -1,42 +1,74 @@
-import requests
-from GabiBraunRobot import dispatcher
-from GabiBraunRobot.modules.disable import DisableAbleCommandHandler
-from telegram import ParseMode, Update
-from telegram.ext import CallbackContext, run_async
+import asyncio
+import os
+import re
+
+import aiofiles
+from pykeyboard import InlineKeyboard
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton
+
+from GabiBraunRobot import aiohttpsession as session
+from GabiBraunRobot import pbot as app
+from GabiBraunRobot.utils.errors import capture_err
+from GabiBraunRobot.utils.pastebin import paste
+
+__mod_name__ = "Paste"
+__help__ = """
+/paste - To Paste Replied Text Or Document To Nekobin
+"""
+pattern = re.compile(
+    r"^text/|json$|yaml$|xml$|toml$|x-sh$|x-shellscript$"
+)
 
 
-@run_async
-def paste(update: Update, context: CallbackContext):
-    args = context.args
-    message = update.effective_message
-
-    if message.reply_to_message:
-        data = message.reply_to_message.text
-
-    elif len(args) >= 1:
-        data = message.text.split(None, 1)[1]
-
-    else:
-        message.reply_text("What am I supposed to do with this?")
-        return
-
-    key = requests.post(
-        'https://nekobin.com/api/documents', json={
-            "content": data
-        }).json().get('result').get('key')
-
-    url = f'https://nekobin.com/{key}'
-
-    reply_text = f'Nekofied to *Nekobin* : {url}'
-
-    message.reply_text(
-        reply_text,
-        parse_mode=ParseMode.MARKDOWN,
-        disable_web_page_preview=True)
+async def isPreviewUp(preview: str) -> bool:
+    for _ in range(7):
+        try:
+            async with session.head(preview, timeout=2) as resp:
+                status = resp.status
+                size = resp.content_length
+        except asyncio.exceptions.TimeoutError:
+            return False
+        if status == 404 or (status == 200 and size == 0):
+            await asyncio.sleep(0.4)
+        else:
+            return True if status == 200 else False
+    return False
 
 
-PASTE_HANDLER = DisableAbleCommandHandler("paste", paste)
-dispatcher.add_handler(PASTE_HANDLER)
+@app.on_message(filters.command("paste") & ~filters.edited)
+@capture_err
+async def paste_func(_, message):
+    if not message.reply_to_message:
+        return await message.reply_text(
+            "Reply To A Message With /paste"
+        )
+    m = await message.reply_text("Pasting...")
+    if message.reply_to_message.text:
+        content = str(message.reply_to_message.text)
+    elif message.reply_to_message.document:
+        document = message.reply_to_message.document
+        if document.file_size > 1048576:
+            return await m.edit(
+                "You can only paste files smaller than 1MB."
+            )
+        if not pattern.search(document.mime_type):
+            return await m.edit("Only text files can be pasted.")
+        doc = await message.reply_to_message.download()
+        async with aiofiles.open(doc, mode="r") as f:
+            content = await f.read()
+        os.remove(doc)
+    link = await paste(content)
+    preview = link + "/preview.png"
+    button = InlineKeyboard(row_width=1)
+    button.add(InlineKeyboardButton(text="Paste Link", url=link))
 
-__command_list__ = ["paste"]
-__handlers__ = [PASTE_HANDLER]
+    if await isPreviewUp(preview):
+        try:
+            await message.reply_photo(
+                photo=preview, quote=False, reply_markup=button
+            )
+            return await m.delete()
+        except Exception:
+            pass
+    return await m.edit(link)
