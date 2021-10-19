d916c98c3f8adce9aad1924ee7545fdf0bb6a5ac
ultroi <86416271+ultroi@users.noreply.github.com>
2021-10-19 05:55:09 +0530

    Update userinfo.py

--- GabiBraunRobot/modules/userinfo.py
+++ GabiBraunRobot/modules/userinfo.py
@@ -313,7 +313,7 @@ def info(update: Update, context: CallbackContext):
         text += "\n\nThe Disaster level of this person is 'Master'."
         disaster_level_present = True
     elif user.id in DEV_USERS:
-        text += "\n\nThis user is Gabi's 'Legend'."
+        text += "\n\nThis user is Pokemon 'Legend'."
         disaster_level_present = True
     elif user.id in DRAGONS:
         text += "\n\nThe Disaster level of this person is 'Professor'."
